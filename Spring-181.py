803024516458619855a5023706ddec9e8be0ef15
Jurijus Pacalovas <pj.midgard@yahoo.com>
2023-10-16 22:20:39 +0100

    Add files via upload

--- Spring-181.py
+++ Spring-181.py
@@ -1,8 +1,8 @@
 import paq
 
 # Initialize variables
-c = X1 = X2 = X3 = X4 = X5 = b = a = X6 = X7 = X8 = X9 = X10 = Y1 = Y2 = Y3 = Y4 = Y5 = Y6 = 0
-Y_counter = 1  # Initialize the counter for Y variables
+c = X1 = X2 = X3 = X4 = X5 = b = a = X6 = X7 = X8 = X9 = X10 = 0
+Y = [0] * 35000  # Initialize a list to store Y values
 
 # Define a function to count 255 and 0 bits
 def count_bits(x):
@@ -16,16 +16,16 @@ def count_x2_repeats(x):
 # Prompt the user for the number of repetitions (number of times to repeat the circle)
 num_repetitions_outer = 1000  # Repeat the circle 1000 times
 
-while Y_counter <= 35:  # Continue until Y35 reaches 311
+for Y_counter in range(1, 35001):  # Loop through Y1 to Y35000
     # Loop based on the number of inner repetitions
     for _ in range(num_repetitions_outer):
-        # Reset variables X1 to X10 and Y1 to Y6 at the beginning of each repetition
-        X1 = X2 = X3 = X4 = X5 = b = a = X6 = X7 = X8 = X9 = X10 = Y1 = Y2 = Y3 = Y4 = Y5 = Y6 = 0
+        # Reset variables X1 to X10 at the beginning of each repetition
+        X1 = X2 = X3 = X4 = X5 = X6 = X7 = X8 = X9 = X10 = 0
 
         num_repetitions_inner = int(input("Enter the number of inner repetitions: "))
 
         for _ in range(num_repetitions_inner):
-            # Get file names for input and output
+            # Ask the user for input and output file names
             input_file_name = input("Enter the name of the input file: ")
             output_file_name = input("Enter the name of the output file: ")
 
@@ -69,26 +69,12 @@ while Y_counter <= 35:  # Continue until Y35 reaches 311
                 X9 += X2  # Extend X9 as needed
                 X10 += X3  # Extend X10 as needed
 
-                # Check Y_counter and increment the corresponding Y variable
-                if Y_counter == 1:
-                    Y1 += 1
-                elif Y_counter == 2:
-                    Y2 += 1
-                elif Y_counter == 3:
-                    Y3 += 1
-                # Continue this pattern for Y4, Y5, and so on up to Y35
-
-                print(f'File {input_file_name} has been {"compressed" if user_option == "1" else "extracted"} and saved as {output_file_name}')
-
-                # Check if the current Y variable has reached 311
-                if Y_counter <= 35 and eval(f'Y{Y_counter}') == 311:
-                    Y_counter += 1
+                # Check if the current Y variable (Y_counter) is 311
+                if Y[Y_counter - 1] == 311:
+                    # Perform specific operations for Y equals 311
+                    print(f"Y{Y_counter} reached 311!")
 
             except FileNotFoundError:
                 print(f"File {input_file_name} not found. Please make sure the file exists.")
 
-# Write results to a file
-with open('x_values.txt', 'w') as file:
-    file.write(f'X1={X1}\nX2={X2}\nX3={X3}\nX4={X4}\nX5={X5}\nX6={X6}\nX7={X7}\nX8={X8}\nX9={X9}\nX10={X10}')
-    file.write(f'Y1={Y1}\nY2={Y2}\nY3={Y3}\nY4={Y4}\nY5={Y5}\nY6={Y6}')
-    # Continue writing Y7, Y8, and so on
+# Now you can access Y values and check if they reach 311
